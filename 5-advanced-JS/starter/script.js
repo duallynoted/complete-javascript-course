850d0308f79bb243db67db332b6919c899ad9e6a
Daniel Ridley <daniel.ridley@bestbuy.com>
2019-09-13 08:52:11 -0500

    More notes

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -40,43 +40,118 @@
 // 	job: 'teacher'
 // };
 
-var Person = function (name, yearOfBirth, job) {
-	this.name = name;
-	this.yearOfBirth = yearOfBirth;
-	this.job = job;
-}
-
-Person.prototype.calcAge =function () {
-	console.log(2019 - this.yearOfBirth)
-}
-
-//this is called instantiation, because these are *instances* of the Person object
-//the word new creates an *empty* object. EMPTY
-//it also points the this variable to that empty object, and away from the global object
-var john = new Person('John', 1990, 'teacher');
-let me = new Person('Daniel', 1980, 'developer', 264)
-let mark = new Person('Mark', 1948, 'retired')
-console.log(john, me, mark)
-me.calcAge()
-mark.calcAge()
-john.calcAge()
-
-
-
-
-
-
-
-
-
-
+// var Person = function (name, yearOfBirth, job) {
+// 	this.name = name;
+// 	this.yearOfBirth = yearOfBirth;
+// 	this.job = job;
+// }
+//
+// Person.prototype.calcAge =function () {
+// 	console.log(2019 - this.yearOfBirth)
+// }
+// Person.prototype.lastName = 'Smith'
+// // this is called instantiation, because these are *instances* of the Person object
+// // the word new creates an *empty* object. EMPTY
+// // it also points the this variable to that empty object, and away from the global object
+// var john = new Person('John', 1990, 'teacher');
+// let me = new Person('Daniel', 1980, 'developer', 264)
+// let mark = new Person('Mark', 1948, 'retired')
+// console.log(john, me, mark)
+// me.calcAge()
+// mark.calcAge()
+// john.calcAge()
+// //
+// let Guitar  = function (brand, model, electric, acoustic, price) {
+// 	this.brand = brand;
+// 	this.model = model;
+// 	this.electric = electric;
+// 	this.acoustic = acoustic;
+// 	this.price = price;
+// }
+//
+// Guitar.prototype.saved = function (sale){
+// 	return this.price - sale
+// }
+//
+// let lesPaul = new Guitar('Gibson', 'Les Paul Studio', true, false, 2000);
+// let tele = new Guitar('Fender', 'Mexican Telecaster', true, false, 1000);
+// let martin = new Guitar('Martin', 'GCPC-A4', false, true, 1699)
+//
+// Guitar.prototype.whoOwnsIt = 'This is my guitar'
+// console.log(lesPaul, tele, martin)
+// console.log('You saved $' + lesPaul.saved(600) + ' on the ' + lesPaul.model + ' by using eBay!')
+// console.log('You saved $' + tele.saved(400) + ' by using eBay!')
+// console.log('You saved $' + martin.saved(1500) + ' by using eBay!')
+//THAT WAS FUN!!!
 
 /**
- Everything is an Object: Prototype and Inheritance Chain
+ Creating Objects: Object.Create
  **/
+
+// let personProto = {
+// 	calcAge: function () {
+// 		console.log(2019 - this.yearOfBirth)
+// 	}
+// }
+//
+// let john = Object.create(personProto)
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = 'teacher';
+//
+// let jane = Object.create(personProto, {
+// 	name: {value: 'Jane'},
+// 	yearOfBirth: {value: 1969},
+// 	job: {value: 'Designer'}
+// })
+//
+let marriedPrototypeProperty = {
+	married: true
+};
+let lastname = {
+	lastName: 'Ridley'
+};
+
+let daniel = Object.assign(marriedPrototypeProperty, lastname, {
+	name: { value: 'Daniel' },
+	fat: { value: true }
+});
+console.log(daniel);
+
 /**
- Everything is an Object: Prototype and Inheritance Chain
+ Primitives vs. Objects
  **/
+//Object variables don't actually hold the data, they point to where the data is stored
+
+let a = 42;
+let b = a;
+b = 95;
+console.log(a, b);
+
+let obj1 = {
+	name: 'John',
+	age: 45
+};
+let obj2 = obj1;
+console.log(obj1.age, obj2.age);
+
+//functions
+
+let age = 27;
+let obj = {
+	name: 'Jonas',
+	city: 'Lisbon'
+};
+
+function change (a, b) {
+	a = 30;
+	b.city = 'San Franciso';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+
 /**
  Everything is an Object: Prototype and Inheritance Chain
  **/
