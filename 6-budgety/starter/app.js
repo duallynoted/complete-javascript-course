da0ccc00c6b38bea95cf548f498d53f06950b8ac
Daniel Ridley <ridleydan31@gmail.com>
2020-01-23 22:46:18 -0600

    added some querySelectors and eventListeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,26 +1,51 @@
+//BUDGET CONTROLLER
 let budgetController = (function () {
-    let x = 23
 
-    let add = function (a) {
-        return x + a
-    }
-    return {
-        publicTest: function (b) {
-            return (add(b))
-        }
-    }
 })();
 
+//UI CONTROLLER
 let uiController = (function () {
-
-})()
-
-let appController = (function (budgetCtrl, uiCtrl) {
-    let z = budgetCtrl.publicTest(32)
-
+    let DOMStrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        button: '.add__btn'
+    }
     return {
-        anotherPublicMethod: function () {
-            console.log(z)
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMStrings.inputType).value, //will be either "inc" or "exp"
+                description: document.querySelector(DOMStrings.inputDescription).value,
+                value: document.querySelector(DOMStrings.inputValue).value
+            }
+
+        },
+        getDOMStrings: function () {
+            return DOMStrings;
         }
     }
+})()
+
+//GLOBAL APP CONTROLLER
+let appController = (function (budgetCtrl, uiCtrl) {
+    let DOM = uiCtrl.getDOMStrings()
+    let ctrlAddItem = function () {
+        //1. Get the field input data
+        let input = uiCtrl.getInput()
+        console.log(input)
+
+        //2. Add item to budget controller
+
+        //3. Add the new item to the UI
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the UI
+    }
+    document.querySelector(DOM.button).addEventListener('click', ctrlAddItem)
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem()
+        }
+    })
 })(budgetController, uiController)
