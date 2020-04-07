81bce333d2dabd508205de7fb9fb5a732188b5c0
Daniel Ridley <daniel.ridley@bestbuy.com>
2020-04-06 23:41:45 -0500

    wrote webpack config and babel config

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,7 @@
 // Global app controller
+import number from './test';
+
+const x = 23;
+
+console.log(`I imported ${number} from another module called test.js.`);
+console.log(('cha cha cha cha changes' + x));
---
+++ 9-forkify/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+	entry: ['babel-polyfill', './starter/src/js/index.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './starter/src/index.html'
+		})
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+};
