56cc6601d6cee9502bf07e4a63d2fd2b611224e5
Daniel Ridley <daniel.ridley@bestbuy.com>
2020-04-08 08:48:40 -0500

    coded search feature for api

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,7 +1,32 @@
-// Global app controller
-import number from './test';
+import Search from './models/Search';
+/*Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes*/
+const state = {}
 
-const x = 23;
+const controlSearch = async () => {
+	// 1. Get query from view
+	const query = 'pizza' //do later
 
-console.log(`I imported ${number} from another module called test.js.`);
-console.log(('cha cha cha cha changes' + x));
+	if(query) {
+		//2. New search object and add to state
+		state.search = new Search(query)
+
+		//3. Prepare UI for results
+
+		//4. Search for recipes
+		await state.search.getResults()
+
+		//5. Render results on UI
+		console.log(state.search.result)
+	}
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault()
+	controlSearch()
+})
+
+search.getResults()
