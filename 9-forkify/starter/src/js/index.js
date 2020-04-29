f9aca9032ac94e3173a006ae8cf86958d1af366e
Daniel Ridley <daniel.ridley@bestbuy.com>
2020-04-28 23:06:17 -0500

    built recipe controller and model

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
+import Recipe from './models/Recipe';
 import { clearLoader, elements, renderLoader } from './views/base';
 /*Global state of the app
 * - Search object
@@ -8,10 +9,10 @@ import { clearLoader, elements, renderLoader } from './views/base';
 * - Liked recipes*/
 const state = {};
 
+/* SEARCH CONTROLLER */
 const controlSearch = async () => {
 	// 1. Get query from view
 	const query = searchView.getInput();
-
 	if (query) {
 		//2. New search object and add to state
 		state.search = new Search(query);
@@ -20,16 +21,18 @@ const controlSearch = async () => {
 		searchView.clearInput();
 		searchView.clearResults();
 		renderLoader(elements.searchRes);
+		try {
 
-		//4. Search for recipes
-		await state.search.getResults();
+			//4. Search for recipes
+			await state.search.getResults();
 
-		//5. Render results on UI
-		clearLoader();
-		searchView.renderResults(state.search.result);
-
-
-
+			//5. Render results on UI
+			clearLoader();
+			searchView.renderResults(state.search.result);
+		} catch (e) {
+			alert('Something went wrong with the search', e);
+			clearLoader();
+		}
 	}
 };
 
@@ -39,14 +42,42 @@ elements.searchForm.addEventListener('submit', e => {
 });
 
 elements.searchResPages.addEventListener('click', e => {
-	const btn = e.target.closest('.btn-inline')
+	const btn = e.target.closest('.btn-inline');
 	if (btn) {
-		const goToPage = parseInt(btn.dataset.goto, 10)
+		const goToPage = parseInt(btn.dataset.goto, 10);
 		searchView.clearResults();
 		searchView.renderResults(state.search.result, goToPage);
 	}
-})
+});
 
+/* RECIPE CONTROLLER */
 
+const controlRecipe = async () => {
+	const id = window.location.hash.replace('#', '');
+	console.log(id);
 
-// search.getResults();
+	if (id) {
+		// Prepare UI for changes
+
+		// Create new recipe object
+		state.recipe = new Recipe(id);
+
+		try {
+			// Get recipe data and parse ingredients
+			await state.recipe.getRecipe();
+			state.recipe.parseIngredients();
+			// Calculate servings and time
+			state.recipe.calcServings();
+			state.recipe.calcTime();
+
+			// Render recipe
+			console.log(state.recipe);
+		} catch (e) {
+			alert('Error processing recipe', e);
+		}
+	}
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+// window.addEventListener('hashchange', controlRecipe)
+// window.addEventListener('load', controlRecipe)
