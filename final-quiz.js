edf632598b6f698b7beef50afa7ae179d3a81e35
Daniel Ridley <daniel.ridley@bestbuy.com>
2020-05-14 21:57:14 -0500

    final exam notes

---
+++ final-quiz.js
@@ -0,0 +1,27 @@
+// !(true or true) and true and true and true  or true or false
+
+// false && true || (true || false)
+
+// false || undefined
+
+// true? look this up later
+
+
+//look up event delegation
+
+//look up the new operator
+
+
+// A = B
+
+// look up why map can be used on arrays --because of the prototype chain? tha fuckever
+
+
+function A () {
+	let X;
+	return function B () {
+	}
+}
+
+
+//look up bind
